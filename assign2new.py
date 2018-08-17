02ec4b8c609c47ae7de706cb01d018326db6f381
bandita <sharmabandita.105@gmail.com>
2018-08-17 12:43:16 +0530

    second commit

---
+++ assign2new.py
@@ -0,0 +1,41 @@
+#Que1-->Print anything you want on screen
+print('I am a student of FALL18_PY_CHITKARA_OFF1')
+print()
+
+#Que2-->Join two strings using '+'
+print('Acad'+'View')
+print()
+
+#Que3-->Take the input of 3 variables x,y and z.Print their values on screen
+x=input("enter the value of first variable")
+y=input("enter the value of second variable")
+z=input("enter the value of third variable")
+print(x,y,z)
+print()
+
+#Que4-->Print "Let's get started" on screen
+print('''"Lets's get started"''')
+print()
+
+"""Que5a-->Print the following values using placeholders
+s="Acadview"
+course="Python"
+fees=5000 """
+print('s="%s"' %"Acadview")
+print('course="%s"' %"Python")
+print('fees=%d' %5000)
+print()
+
+"""Que5b-->Let's do some interseting exercise:
+name="Tony Stark"
+salary=1000000
+print('%s''%d')%(____,____)"""
+name="Tony Stark"
+salary=1000000
+print('%s''%d' %(name,salary))
+print()
+
+#Que6-->Find the area of circle user defined radius
+r=int(input("enter the radius of cicle:"))
+area=3.14*r*r
+print("the area of circle is:",area)
